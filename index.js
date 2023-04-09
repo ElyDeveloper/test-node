340a63121b8ce44fc8742b774fe91f7a624cf80a
ElyDeveloper <57238178+ElyDeveloper@users.noreply.github.com>
2023-04-08 22:32:36 -0600

    Update index
    Update index

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// const express = require("express");
+const express = require("express");
 
-// const bodyParse = require("body-parser");
+const bodyParse = require("body-parser");
 
-// const app = express();
-// const PORT = 3000;
-
-// app.use(bodyParse.urlencoded({ extended: false }));
-// app.use(bodyParse.json());
-
-// app.get("/", (req, res) => {
-
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World!");
-// });
-
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-const http = require("http");
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+
+app.get("/", (req, res) => {
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello World!");
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+
+// const http = require("http");
+// const PORT = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World!");
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}/`);
+// });
